61cad119b2dcdbbd31891d4f2bc9e36a99cc4c87
Minbu5 <128700993+Minbu5@users.noreply.github.com>
2025-01-30 20:28:42 +0200

    Add main puzzle logic and solutions

--- Bottle.py
+++ Bottle.py
@@ -1,8 +1,82 @@
 
 
-class Bottle:
-    def __init__(self, volume, content):
-        self.volume = volume
-        self.content = content
+bottle_3 = {"max quantity": 3,
+         "content": 0,
+         "empty space": 3
+         }
+bottle_5 = {"max quantity": 5,
+         "content": 0,
+         "empty space": 5
+         }
+
+def check_solve(bottle):
+    if bottle["content"] == 4:
+        print("*******************")
+        print("**     !!!!!     **")
+        print("** !!! WOHOO !!! **")
+        print("**     !!!!!     **")
+        print("*******************")
+
+def fill(bottle):
+    bottle["content"] = bottle["max quantity"]
+    bottle["empty space"] = 0
 
 
+def spill(bottle):
+    bottle["content"] = 0
+    bottle["empty space"] = bottle["max quantity"]
+
+def transfer(bottle_from, botttle_to):
+    if bottle_from["content"] >= botttle_to["empty space"]:
+        transfer_quantity = bottle_from["content"]-(bottle_from["content"] - botttle_to["empty space"])# (5 - (5 - 3)  = 3
+
+    else:
+        transfer_quantity = bottle_from["content"]
+
+    bottle_from["content"] -= transfer_quantity
+    botttle_to["content"] += transfer_quantity
+    bottle_from["empty space"] += transfer_quantity
+    botttle_to["empty space"] -= transfer_quantity
+
+
+
+# solution 1
+###########################
+# fill(bottle_5)
+# transfer(bottle_5, bottle_3)
+# spill((bottle_3))
+# transfer(bottle_5, bottle_3)
+# fill(bottle_5)
+# transfer(bottle_5, bottle_3)
+
+
+# solution 2
+#############################
+# fill(bottle_3)
+# transfer(bottle_3, bottle_5)
+# fill(bottle_3)
+# transfer(bottle_3, bottle_5)
+# spill(bottle_5)
+# transfer(bottle_3, bottle_5)
+# fill(bottle_3)
+# transfer(bottle_3, bottle_5)
+
+
+
+
+# transfer(bottle_3, bottle_5)
+#
+# spill(bottle_5)
+
+# test
+print("*******************************")
+print(f"Content b5: {bottle_5["content"]}")
+print(f"Free space b5: {bottle_5["empty space"]}")
+print("*******************************")
+print("*******************************")
+print(f"Content b3: {bottle_3["content"]}")
+print(f"Free space b3: {bottle_3["empty space"]}")
+print("*******************************")
+print("*******************************")
+
+check_solve(bottle_5)
--- Game.py
+++ Game.py
@@ -1,5 +1,8 @@
 
 
+
+
+
 def run_game(bot_5, bot_3 ):
     sel_bot = input(f"In 5 l. bottle is {bot_5} l.\nIn in 3 l. bottle is {bot_3} l.\n"
                     f"Select bottle: \n5 -  for 5 l.,  \n3 - for 3 l.:\n")
@@ -11,3 +14,4 @@ def run_game(bot_5, bot_3 ):
     print("******************************************************************")
 
     return action
+
